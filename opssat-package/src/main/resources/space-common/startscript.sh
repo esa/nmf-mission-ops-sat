94a1fb77142c76d3fe5821d037c547c0f8685e3a
Dominik Marszk <dominik.marszk@esa.int>
2019-04-10 18:39:10 +0000

    [opssat-package] Factor out app assembler, replace with ant and custom script

---
+++ opssat-package/src/main/resources/space-common/startscript.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# NMF_LIB can be provided by the parent app (i.e. supervisor) or set locally
+if [ -z "$NMF_LIB" ] ; then
+    NMF_LIB=@NMF_LIB@
+fi
+
+# Replaced with the main class name
+MAIN_CLASS_NAME=@MAIN_CLASS_NAME@
+
+exec java $JAVA_OPTS \
+  -classpath "$NMF_LIB:lib/*" \
+  "$MAIN_CLASS_NAME" \
+  "$@"
+
