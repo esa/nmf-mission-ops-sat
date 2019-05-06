e81bd80e7cc893e6c5d9ade181b759c21a115edd
Yannick Lavan <yannick.lavan@esa.int>
2019-05-06 12:30:30 +0000

    Feature #415: Implemented unset method for iADCS.
    The current attitude mode can now be unset by calling the unset() method in the adapter.

--- platform-service-impl/src/main/java/esa/mo/platform/impl/provider/opssat/AutonomousADCSOPSSATAdapter.java
+++ platform-service-impl/src/main/java/esa/mo/platform/impl/provider/opssat/AutonomousADCSOPSSATAdapter.java
@@ -356,8 +356,6 @@ public class AutonomousADCSOPSSATAdapter implements AutonomousADCSAdapterInterfa
         throw new UnsupportedOperationException("Only X, Y and Z are valid Single Spinning axis.");
       }
       adcsApi.Start_SingleAxis_AngularVelocity_Controller(vec, a.getAngularVelocity());
-      adcsApi.Start_SingleAxis_AngularVelocity_Controller(
-          SEPP_IADCS_API_SINGLEAXIS_CONTROL_TARGET_AXIS.IADCS_SINGLEAXIS_CONTROL_TARGET_X, 0);
       activeAttitudeMode = a;
     } else if (attitude instanceof AttitudeModeSunPointing) {
       if (targetVector == null) {
@@ -426,6 +424,8 @@ public class AutonomousADCSOPSSATAdapter implements AutonomousADCSAdapterInterfa
       params.setSTOP_LONGITUDE(a.getLongitude_end());
       params.setTOLERANCE_PARAMETERS(tolerance);
       params.setUPDATE_INTERVAL(BigInteger.valueOf(500));
+      adcsApi.Set_Epoch_Time(BigInteger.valueOf(System.currentTimeMillis()));
+      adcsApi.Init_Orbit_Module(readTLEFile());
       adcsApi.Start_Target_Pointing_Earth_Const_Velocity_Mode(params);
       activeAttitudeMode = a;
     } else {
@@ -448,7 +448,6 @@ public class AutonomousADCSOPSSATAdapter implements AutonomousADCSAdapterInterfa
       adcsApi.Stop_SingleAxis_AngularVelocity_Controller(
           SEPP_IADCS_API_SINGLEAXIS_CONTROL_TARGET_AXIS.IADCS_SINGLEAXIS_CONTROL_TARGET_Y);
     } else if (activeAttitudeMode instanceof AttitudeModeSunPointing) {
-
       adcsApi.Stop_Operation_Mode_Sun_Pointing();
     } else if (activeAttitudeMode instanceof AttitudeModeTargetTracking) {
       adcsApi.Stop_Target_Pointing_Earth_Fix_Mode();
