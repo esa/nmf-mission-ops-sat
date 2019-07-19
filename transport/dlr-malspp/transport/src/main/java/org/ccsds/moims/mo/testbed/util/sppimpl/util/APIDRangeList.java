ecdd3bbb725ac84cc7bfb3858c8021cc03201fec
Yannick Lavan <yannick.lavan@esa.int>
2019-07-19 09:50:53 +0000

    Bug #331/#500: Implement APID whitelisting
    Implement APID whitelisting for space packet
    processing and CRC checking. Use crc_apids.txt and
    processed_apids.txt to specify the ranges.

---
+++ transport/dlr-malspp/transport/src/main/java/org/ccsds/moims/mo/testbed/util/sppimpl/util/APIDRangeList.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ccsds.moims.mo.testbed.util.sppimpl.util;
+
+import java.util.ArrayList;
+
+/**
+ * List of APID ranges used to form a composite range of possible APIDs consisting of several intervals.
+ * @author yannick
+ */
+public class APIDRangeList extends ArrayList<APIDRange>
+{
+  /**
+   * Checks if a specified APID is inside the range of this list.
+   * @param apid The APID to check.
+   * @return True iff there exists a <i>range</i> in <b>this</b> with range.inRange(apid).
+   */
+  public boolean inRange(int apid){
+    for(APIDRange r : this){
+      if(r.inRange(apid)){
+        return true;
+      }
+    }
+    return false;
+  }
+}
