655d6b050d340470cf537ce43a13099c4a9aaaee
Kevin Otto <Kevin@KevinOtto.de>
2020-01-30 08:56:47 +0000

    [GPS OPSSAT Adapter] Feature #723: Fix catch clause in getNMEASentence

--- platform-service-impl/src/main/java/esa/mo/platform/impl/provider/opssat/GPSOPSSATAdapter.java
+++ platform-service-impl/src/main/java/esa/mo/platform/impl/provider/opssat/GPSOPSSATAdapter.java
@@ -30,45 +30,63 @@ import org.ccsds.moims.mo.mal.MALException;
 import esa.opssat.nanomind.opssat_pf.gps.consumer.GPSAdapter;
 import org.ccsds.moims.mo.mal.MALInteractionException;
 import org.ccsds.moims.mo.mal.structures.Blob;
+import org.ccsds.moims.mo.platform.gps.structures.TwoLineElementSet;
 
 /**
  *
  * @author Cesar Coelho
  */
-public class GPSOPSSATAdapter extends GPSNMEAonlyAdapter {
+public class GPSOPSSATAdapter extends GPSNMEAonlyAdapter
+{
 
-    private final NanomindServicesConsumer obcServicesConsumer;
+  private final NanomindServicesConsumer obcServicesConsumer;
 
-    public GPSOPSSATAdapter(NanomindServicesConsumer obcServicesConsumer) {
-        this.obcServicesConsumer = obcServicesConsumer;
+  public GPSOPSSATAdapter(NanomindServicesConsumer obcServicesConsumer)
+  {
+    this.obcServicesConsumer = obcServicesConsumer;
+  }
+
+  @Override
+  public String getNMEASentence(String identifier) throws IOException
+  {
+    GPSHandler gpsHandler = new GPSHandler();
+    try {
+      obcServicesConsumer.getGPSNanomindService().getGPSNanomindStub().getGPSData(new Blob(
+          identifier.getBytes()), gpsHandler);
+    } catch (MALInteractionException | MALException ex) {
+      Logger.getLogger(GPSOPSSATAdapter.class.getName()).log(Level.SEVERE, null, ex);
+      throw new IOException("Error when retrieving GPS NMEA response from Nanomind", ex);
     }
+    return gpsHandler.response;
+  }
+
+  @Override
+  public boolean isUnitAvailable()
+  {
+    return true;
+  }
+
+  @Override
+  public TwoLineElementSet getTLE()
+  {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  private class GPSHandler extends GPSAdapter
+  {
+
+    String response = "";
 
     @Override
-    public String getNMEASentence(String identifier) throws IOException {
-        GPSHandler gpsHandler = new GPSHandler();
-        try {
-            obcServicesConsumer.getGPSNanomindService().getGPSNanomindStub().getGPSData(new Blob(identifier.getBytes()), gpsHandler);
-        } catch (IllegalArgumentException | MALException | MALInteractionException e) {
-            throw new IOException("Error when retrieving GPS NMEA response from Nanomind", e);
-        }
-        return gpsHandler.response;
+    public void getGPSDataResponseReceived(
+        org.ccsds.moims.mo.mal.transport.MALMessageHeader msgHeader,
+        org.ccsds.moims.mo.mal.structures.Blob data, java.util.Map qosProperties)
+    {
+      try {
+        response = Arrays.toString(data.getValue());
+      } catch (MALException ex) {
+        Logger.getLogger(GPSHandler.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
-
-    @Override
-    public boolean isUnitAvailable() {
-        return true;
-    }
-
-    private class GPSHandler extends GPSAdapter {
-        String response = "";
-        @Override
-        public void getGPSDataResponseReceived(org.ccsds.moims.mo.mal.transport.MALMessageHeader msgHeader,
-                org.ccsds.moims.mo.mal.structures.Blob data, java.util.Map qosProperties) {
-            try {
-                response = Arrays.toString(data.getValue());
-            } catch (MALException ex) {
-                Logger.getLogger(GPSHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
+  }
 }
