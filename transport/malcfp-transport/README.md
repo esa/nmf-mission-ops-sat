ef6e873dd9c7b0d69189a5aaade7f7032f40354f
Dominik Marszk <dominik.marszk@esa.int>
2020-07-30 12:03:08 +0000

    [transport] Cleanup unused files (CFP, Kayak)

--- transport/malcfp-transport/README.md
+++
@@ -1,4 +0,0 @@
-MAL binding to CAN Fragmentation Protocol
-=====
-
-Note that this transport is highly experimental and NOT being used by OPS-SAT.
--- transport/malcfp-transport/src/main/java/esa/mo/mal/transport/cfp/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
-An implementation of the transport interfaces for the CAN link. Builds upon the generic transport framework.
- */
-package esa.mo.mal.transport.cfp;
